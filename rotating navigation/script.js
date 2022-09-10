1bc643b048559e2e7d1eeaec7e18af2192abee9f
adewaleracheal <107050429+i759@users.noreply.github.com>
2022-09-10 20:09:15 +0100

    Deleted flaticon icons , used Font-awesome, Added CSS and JavaScript.

--- rotating navigation/script.js
+++ rotating navigation/script.js
@@ -1,3 +1,6 @@
-module.exports = {
-  multipass: true, // boolean. false by default
-};
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+close.addEventListener('click', () => container.classList.remove('show-nav'))
